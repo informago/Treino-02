4a01879f8cac03b5c501ddefcef6ca5822341673
informago <luciano@larsoft.com.br>
2024-10-29 00:06:42 -0300

    Primeiro controller e model

---
+++ Treino REST 02/Controllers/T01Controller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Treino_REST_02.Models;
+
+namespace Treino_REST_02.Controllers
+{
+    [Route("api/Operacao")]
+    [ApiController]
+    public class SomaController : ControllerBase
+    {
+
+        [HttpPost(Name = "Soma")]
+        public int Soma(int A, int B) 
+        {
+            return A + B;
+        }
+
+    }
+
+    [Route("api/ListaUF")]
+    [ApiController]
+    public class ListaUFController : ControllerBase
+    {
+
+        [HttpGet(Name = "Divisão")]
+        public IEnumerable<UF> ListaUF()
+        {
+            return new List<UF>
+            {
+                new UF {Id = 1, Nome = "RJ", Capital = "Rio de Janeiro" },
+                new UF {Id = 2, Nome = "SP", Capital = "São Paulo" },
+                new UF {Id = 3, Nome = "BA", Capital = "Salvador" }
+            };
+        }
+    }
+}
---
+++ Treino REST 02/Models/UF.cs
@@ -0,0 +1,9 @@
+﻿namespace Treino_REST_02.Models
+{
+    public class UF
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public string? Capital { get; set; }
+    }
+}
