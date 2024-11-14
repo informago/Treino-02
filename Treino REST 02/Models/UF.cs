3fc516707c6b17ed1554c4bcc5ce33047dba171e
informago <luciano@larsoft.com.br>
2024-11-13 21:44:12 -0300

    Validação no Model

--- Treino REST 02/Models/UF.cs
+++ Treino REST 02/Models/UF.cs
@@ -1,9 +1,14 @@
-﻿namespace Treino_REST_02.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Treino_REST_02.Models
 {
     public class UF
     {
+        [Required]
         public int Id { get; set; }
+        [Required][MaxLength(2)]
         public required string Nome { get; set; }
+        [Required][MaxLength(50)]
         public string? Capital { get; set; }
     }
 }
